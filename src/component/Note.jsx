7d8ec09b130a31a0acdf2347847655ec60f1aed7
krunalbhongade <krunal.bhongade@gmail.com>
2023-09-04 15:35:36 +0530

    keeper app part 1

---
+++ src/component/Note.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Note () {
+  return (  
+    <div className="note">
+        <h1>For title</h1>
+        <p>For the content.</p>
+    </div>
+  );
+}
+
+export default Note;
